25c33143f4b0143e1233a4b0083ed45b889690d1
harbargerdev <aharbarger@gmail.com>
2020-07-27 20:28:45 -0400

    Implementing randomize questions

--- RandomQuestion.Core/Utilities/QuestionUtility.cs
+++ RandomQuestion.Core/Utilities/QuestionUtility.cs
@@ -1,10 +1,38 @@
-﻿namespace RandomQuestion.Core.Utilities
+﻿using RandomQuestion.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace RandomQuestion.Core.Utilities
 {
     public interface IQuestionUtility
     {
+        /// <summary>
+        /// Method that randomizes questions from the question bank and assigns it to the Questions property
+        /// </summary>
+        /// <param name="questionaire">The questionaire to be randomized</param>
+        void RandomizeQuestions(Questionaire questionaire);
     }
 
     public class QuestionUtility : IQuestionUtility
     {
+        public void RandomizeQuestions(Questionaire questionaire)
+        {
+            // Verify inputs
+            if (questionaire.QuestionCount == 0 
+                || questionaire.QuestionBank.Questions == null
+                || questionaire.QuestionBank.Questions.Count == 0)
+                return;
+
+            var random = new Random();
+            var questions = new List<Question>();
+            var initialList = new List<Question>(questionaire.QuestionBank.Questions);
+
+            for (int i = 0; i < questionaire.QuestionCount; i++)
+            {
+                var position = random.Next(0, initialList.Count - 1);
+                questions.Add(initialList[position]);
+                initialList.RemoveAt(position);
+            }
+        }
     }
 }
